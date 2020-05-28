c5fcffa352d2acc04878281ebf72c12c441b5024
PBMocha <joshuabrandonpb@gmail.com>
2020-05-28 09:10:33 -0400

    Added relationships to Post model

--- pigeon-backend/app/Models/Post.php
+++ pigeon-backend/app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -9,5 +10,17 @@ class Post extends Model
     //
 
     protected $fillable = ['title', 'body', 'user_id', 'image_id', 'likes'];
+
+    //TODO: Create relationship with users
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    //TODO: Create relationship with profile
+
+    //TODO: Create relationship with comments
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
 
 }
